45d8f8f45901cf39ec159987f33577f72b490970
rubenmedgon <a23310341@live.ceti.mx>
2024-11-25 14:47:31 -0600

    prueba_03

--- src/00_Main.cpp
+++ src/00_Main.cpp
@@ -1,20 +1,40 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <vector>
+#include <time.h>
+#include "../include/Ficha.hpp"
 
 using namespace std;
 using namespace sf;
 
 int main(){
     //Ventana
-    RenderWindow window(VideoMode(850,750),"Jodo");
+    RenderWindow window(VideoMode(2000,900),"Jodo");
     //Fondo
     Texture texture;
-    if(!texture.loadFromFile("./assets/Carta_01.png")){
+    if(!texture.loadFromFile("./assets/Salon.png")){
         return -1;
     }
-    
     Sprite sprite(texture);
+    //Fichas
+    vector<Texture> textures(2);
+    for(int i=1; i<=2; i++){
+        if(!textures[i-1].loadFromFile(to_string(i)+".png")){
+        return -1;
+        }
+    }
+    vector<vector<Ficha>> ficha(1,vector<Ficha>(4,Ficha(0,0,0)));
+    vector<int> cantApariciones(6,0);
+    int tipoRandom;
 
+    srand(time(NULL));
+    for(int j=0; j<4; j++){
+        do{
+            tipoRandom=1+rand()%6;
+        }while(cantApariciones[tipoRandom-1]==2);
+        cantApariciones[tipoRandom-1]++;
+    }
+    
     while(window.isOpen()){
         Event event;
         while(window.pollEvent(event)){
