494ce0b51da1881e5b688ecb0e78ad6a73a447a4
rubenmedgon <a23310341@live.ceti.mx>
2024-11-24 23:38:38 -0600

    prueba_02

--- src/00_Main.cpp
+++ src/00_Main.cpp
@@ -5,14 +5,23 @@ using namespace std;
 using namespace sf;
 
 int main(){
-    RenderWindow window(VideoMode(850,750),"Memotest");
+    //Ventana
+    RenderWindow window(VideoMode(850,750),"Jodo");
+    //Fondo
+    Texture texture;
+    if(!texture.loadFromFile("./assets/Carta_01.png")){
+        return -1;
+    }
     
+    Sprite sprite(texture);
+
     while(window.isOpen()){
         Event event;
         while(window.pollEvent(event)){
             if(event.type==Event::Closed) window.close();
         }
         window.clear();
+        window.draw(sprite);
         window.display();
     }
     return 0;
